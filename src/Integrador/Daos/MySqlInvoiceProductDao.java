2ce82b2009bc8cdbedf164f1feb7486fd481571f
Ezequiel Cinalli <ezequielcinalli96@gmail.com>
2022-09-05 21:48:18 -0300

    Added InvoiceProductDao implementation
    Mysql CRUD implementation tested with missing service3 query, derby only copy-parte without working and test

--- src/Integrador/Daos/MySqlInvoiceProductDao.java
+++ src/Integrador/Daos/MySqlInvoiceProductDao.java
@@ -1,7 +1,10 @@
 package Integrador.Daos;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -25,27 +28,76 @@ public class MySqlInvoiceProductDao implements IInvoiceProductDao {
     }
 
     @Override
+    public Optional<InvoiceProduct> get(int invoiceId, int productId) {
+        InvoiceProduct result = null;
+        String select = "SELECT * FROM " + entityName + " WHERE invoiceId = ? AND productId = ?";
+        try (PreparedStatement ps = connection.prepareStatement(select)) {
+            ps.setInt(1, invoiceId);
+            ps.setInt(2, productId);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next())
+                result = createEntityFromResultSet(rs);
+            connection.commit();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return result == null ? Optional.empty() : Optional.of(result);
+    }
+
+    @Override
     public List<InvoiceProduct> getAll() {
-        // TODO Auto-generated method stub
-        return null;
+        List<InvoiceProduct> result = new ArrayList<InvoiceProduct>();
+        String select = "SELECT * FROM " + entityName;
+        try (PreparedStatement ps = connection.prepareStatement(select)) {
+            ResultSet rs = ps.executeQuery();
+            while (rs.next())
+                result.add(createEntityFromResultSet(rs));
+            connection.commit();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return result;
     }
 
     @Override
     public void save(InvoiceProduct t) {
-        // TODO Auto-generated method stub
-
+        String select = "INSERT INTO " + entityName + " (invoiceId,productId,amount) VALUES (?,?,?)";
+        try (PreparedStatement ps = connection.prepareStatement(select)) {
+            ps.setInt(1, t.getInvoiceId());
+            ps.setInt(2, t.getProductId());
+            ps.setInt(3, t.getAmount());
+            ps.executeUpdate();
+            connection.commit();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void update(InvoiceProduct t) {
-        // TODO Auto-generated method stub
-
+        String select = "UPDATE " + entityName + " SET amount=?  WHERE invoiceId=? AND productId=?";
+        try (PreparedStatement ps = connection.prepareStatement(select)) {
+            ps.setInt(1, t.getAmount());
+            ps.setInt(2, t.getInvoiceId());
+            ps.setInt(3, t.getProductId());
+            ps.executeUpdate();
+            connection.commit();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void delete(InvoiceProduct t) {
-        // TODO Auto-generated method stub
-
+        String select = "DELETE FROM " + entityName + " WHERE invoiceId=? AND productId=?";
+        try (PreparedStatement ps = connection.prepareStatement(select)) {
+            ps.setInt(1, t.getInvoiceId());
+            ps.setInt(2, t.getProductId());
+            ps.executeUpdate();
+            connection.commit();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -67,10 +119,23 @@ public class MySqlInvoiceProductDao implements IInvoiceProductDao {
         }
     }
 
-	@Override
-	public Product getMostBilledProduct() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public Product getMostBilledProduct() {
+        Product result = null;
+        String select = "SELECT * FROM product LIMIT 1";
+        try (PreparedStatement ps = connection.prepareStatement(select)) {
+            ResultSet rs = ps.executeQuery();
+            if (rs.next())
+                result = new Product(rs.getInt(1), rs.getString(2), rs.getInt(3));
+            connection.commit();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    private InvoiceProduct createEntityFromResultSet(ResultSet rs) throws SQLException {
+        return new InvoiceProduct(rs.getInt(1), rs.getInt(2), rs.getInt(3));
+    }
 
 }
--- src/Integrador/Interfaces/IInvoiceProductDao.java
+++ src/Integrador/Interfaces/IInvoiceProductDao.java
@@ -1,8 +1,14 @@
 package Integrador.Interfaces;
 
+import java.util.Optional;
+
 import Integrador.Models.InvoiceProduct;
 import Integrador.Models.Product;
 
 public interface IInvoiceProductDao extends IDao<InvoiceProduct> {
-	public Product getMostBilledProduct();
+    public String entityName = "invoice_product";
+
+    public Optional<InvoiceProduct> get(int invoiceId, int productId);
+
+    public Product getMostBilledProduct();
 }
