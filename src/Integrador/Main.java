cdd713f593602583e1c52d62d169ca3ec692b68b
German De Francesco <germandefrancesco@hotmail.com>
2022-09-07 19:04:57 -0300

    Insert lines read from CSVs in the database

--- src/Integrador/Main.java
+++ src/Integrador/Main.java
@@ -4,8 +4,12 @@ import java.util.ArrayList;
 
 import Integrador.Interfaces.DaoFactory;
 import Integrador.Interfaces.ICustomerDao;
+import Integrador.Interfaces.IInvoiceDao;
 import Integrador.Interfaces.IInvoiceProductDao;
+import Integrador.Interfaces.IProductDao;
 import Integrador.Models.Customer;
+import Integrador.Models.Invoice;
+import Integrador.Models.InvoiceProduct;
 import Integrador.Models.Product;
 import Integrador.Services.CsvReader;
 
@@ -46,17 +50,34 @@ public class Main {
     public static void seedData(DaoFactory daoFactory) {
     	final String basePath = "src\\Integrador\\Data\\";
     	
+    	ICustomerDao customerDao = daoFactory.getCustomerDao();
     	ArrayList<String[]> customers = CsvReader.ReadLinesFromFilePath(basePath + "Customers.csv");
-    	// TODO insert in database
+    	for (String[] customerParams : customers) {
+    		Customer customer = new Customer(Integer. parseInt(customerParams[0]), customerParams[1], customerParams[2]);
+    		customerDao.save(customer);
+    	}
     	
-    	ArrayList<String[]> invoiceProducts = CsvReader.ReadLinesFromFilePath(basePath + "InvoiceProducts.csv");
-    	// TODO insert in database
-    	
+    	IInvoiceDao invoiceDao = daoFactory.getInvoiceDao();
     	ArrayList<String[]> invoices = CsvReader.ReadLinesFromFilePath(basePath + "Invoices.csv");
-    	// TODO insert in database
+		for (String[] invoiceParams : invoices) {
+			Invoice invoice = new Invoice(Integer. parseInt(invoiceParams[0]), Integer. parseInt(invoiceParams[1]));
+			invoiceDao.save(invoice);
+    	}
     	
+    	IProductDao productDao = daoFactory.getProductDao();
     	ArrayList<String[]> products = CsvReader.ReadLinesFromFilePath(basePath + "Products.csv");
-    	// TODO insert in database
+    	for (String[] productParams : products) {
+    		Product product = new Product(Integer. parseInt(productParams[0]), productParams[1], Float.parseFloat(productParams[2]));
+    		productDao.save(product);
+    	}
+    	
+    	IInvoiceProductDao invoiceProductDao = daoFactory.getInvoiceProductDao();
+    	ArrayList<String[]> invoiceProducts = CsvReader.ReadLinesFromFilePath(basePath + "InvoiceProducts.csv");
+    	for (String[] invoiceProductParams : invoiceProducts) {
+    		InvoiceProduct invoiceProduct = new InvoiceProduct(
+				Integer. parseInt(invoiceProductParams[0]), Integer. parseInt(invoiceProductParams[1]), Integer. parseInt(invoiceProductParams[2]));
+    		invoiceProductDao.save(invoiceProduct);
+    	}
     }
 
 }
