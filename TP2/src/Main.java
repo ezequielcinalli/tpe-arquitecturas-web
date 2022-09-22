70015f02f2882d4d4fa18bd30f631973eb430479
Ezequiel Cinalli <ezequielcinalli96@gmail.com>
2022-09-22 18:41:57 -0300

    Improved main messages

--- TP2/src/Main.java
+++ TP2/src/Main.java
@@ -10,7 +10,24 @@ public class Main {
 		entityManager.getTransaction().begin();
 
 		//Code for services 
-		System.out.println("Hello world");
+		System.out.println("--------------Dar de alta un estudiante--------------") ;
+
+		System.out.println("--------------Matricular un estudiante en una carrera--------------") ;
+
+		System.out.println("--------------Recuperar todos los estudiantes, y especificar algún criterio de ordenamiento simple.--------------") ;
+
+		System.out.println("--------------Recuperar un estudiante, en base a su número de libreta universitaria.--------------") ;
+
+		System.out.println("--------------Recuperar todos los estudiantes, en base a su género.--------------") ;
+
+		System.out.println("--------------Recuperar las carreras con estudiantes inscriptos, y ordenar por cantidad de inscriptos.--------------") ;
+
+		System.out.println("--------------Recuperar los estudiantes de una determinada carrera, filtrado por ciudad de residencia--------------") ;
+
+		System.out.println("-------------- Generar un reporte de las carreras, que para cada carrera incluya información de los\n"
+				+ "inscriptos y egresados por año. Se deben ordenar las carreras alfabéticamente, y presentar\n"
+				+ "los años de manera cronológica.--------------") ;
+		
 
 		entityManager.getTransaction().commit();
 		entityManager.close();
