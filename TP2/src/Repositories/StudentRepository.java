94af732d262fabb8d3ebdad5ecff3fc47eaa2ac2
Ezequiel Cinalli <ezequielcinalli96@gmail.com>
2022-09-22 18:49:35 -0300

    Added repositories with default implementation

---
+++ TP2/src/Repositories/StudentRepository.java
@@ -0,0 +1,40 @@
+package Repositories;
+
+import java.util.List;
+
+import Interfaces.IStudentRepository;
+import Models.Student;
+
+public class StudentRepository implements IStudentRepository{
+
+	@Override
+	public Student get(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Student> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void save(Student t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Student t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(Student t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
