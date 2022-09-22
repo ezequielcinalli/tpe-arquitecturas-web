9a1bc6b851e3ac2cb62ccbde32937de191558506
Ezequiel Cinalli <ezequielcinalli96@gmail.com>
2022-09-22 12:39:53 -0300

    Improved TP2 pom and persistence.xml

--- TP2/src/Main.java
+++ TP2/src/Main.java
@@ -1,9 +1,20 @@
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("MySql");
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		entityManager.getTransaction().begin();
+
+		//Code for services 
 		System.out.println("Hello world");
+
+		entityManager.getTransaction().commit();
+		entityManager.close();
+		entityManagerFactory.close();
 	}
 
 }
