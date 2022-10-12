700015c4c32cf6057c316f5585ba3667d656ccf3
carohansen73 <laaflaka.8@hotmail.com>
2022-10-12 10:39:25 -0300

    Add career seed

--- TP3/src/main/java/com/example/tp3/Utils/LoadDatabase.java
+++ TP3/src/main/java/com/example/tp3/Utils/LoadDatabase.java
@@ -1,8 +1,10 @@
 package com.example.tp3.Utils;
 
+import com.example.tp3.Models.Career;
 import com.example.tp3.Models.City;
 import com.example.tp3.Models.Genre;
 import com.example.tp3.Models.Student;
+import com.example.tp3.Repositories.CareerRepository;
 import com.example.tp3.Repositories.CityRepository;
 import com.example.tp3.Repositories.StudentRepository;
 
@@ -19,7 +21,7 @@ import org.springframework.context.annotation.Configuration;
 class LoadDatabase {
 
     @Bean
-        CommandLineRunner initDatabase(CityRepository cityRepository, StudentRepository studentRepository) {
+        CommandLineRunner initDatabase(CityRepository cityRepository, StudentRepository studentRepository, CareerRepository careerRepository) {
         return args -> {
         	var city1 = new City("City1");
         	var city2 = new City("City2");
@@ -29,6 +31,27 @@ class LoadDatabase {
             var student2 = new Student("Student2", "Student2", new Date(2000-01-01), Genre.Femenino, 123456, city2);
             log.info("Preloading " + studentRepository.save(student1));
             log.info("Preloading " + studentRepository.save(student2));
+            var career1 = new Career("Arquitecturas web");
+            var career2 = new Career("Quimica");
+            var career3 = new Career("Matematicas");
+            var career4 = new Career("Programacion");
+            var career5 = new Career("Administracion de empresas");
+            var career6 = new Career("Desarrrollo web");
+            var career7 = new Career("Literatura");
+            var career8 = new Career("Profesorado ingles");
+            var career9 = new Career("Arte");
+            var career10 = new Career("Fisica");
+            log.info("Preloading " + careerRepository.save(career1));
+            log.info("Preloading " + careerRepository.save(career2));
+            log.info("Preloading " + careerRepository.save(career3));
+            log.info("Preloading " + careerRepository.save(career4));
+            log.info("Preloading " + careerRepository.save(career5));
+            log.info("Preloading " + careerRepository.save(career6));
+            log.info("Preloading " + careerRepository.save(career7));
+            log.info("Preloading " + careerRepository.save(career8));
+            log.info("Preloading " + careerRepository.save(career9));
+            log.info("Preloading " + careerRepository.save(career10));
+
         };
     }
 }
