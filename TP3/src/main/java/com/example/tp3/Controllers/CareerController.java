41b722594d1a97367313445f2be3823d7384dc96
Ezequiel Cinalli <ezequielcinalli96@gmail.com>
2022-10-13 09:38:26 -0300

    Fixed typo error

--- TP3/src/main/java/com/example/tp3/Controllers/CareerController.java
+++ TP3/src/main/java/com/example/tp3/Controllers/CareerController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("carees")
+@RequestMapping("careers")
 public class CareerController {
     
     @Autowired
