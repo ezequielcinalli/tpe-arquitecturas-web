a7feeeb4512f411d8ca85d91ebcd4a8358440d11
Ezequiel Cinalli <ezequielcinalli96@gmail.com>
2022-10-13 18:21:24 -0300

    Added middleware for foreign key constraint error

--- TP3/src/main/java/com/example/tp3/Middlewares/CustomControllerAdvice.java
+++ TP3/src/main/java/com/example/tp3/Middlewares/CustomControllerAdvice.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.example.tp3.Dtos.ErrorDto;
 
+import java.sql.SQLIntegrityConstraintViolationException;
+
 @RestControllerAdvice
 class CustomControllerAdvice {
 	
@@ -21,5 +23,19 @@ class CustomControllerAdvice {
             status
         );
     }
+
+    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
+    public ResponseEntity<ErrorDto> handleJdbcSQLIntegrityConstraintViolationException(
+            SQLIntegrityConstraintViolationException e) {
+        e.printStackTrace();
+        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+        return new ResponseEntity<ErrorDto>(
+                new ErrorDto(e.getMessage()),
+                status
+        );
+    }
 	
 }
+
+
+
