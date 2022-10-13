84b47b34bcec832d1f553a3edbf061299506b58b
Lucas Guerrero <guerrerolucas246@gmail.com>
2022-10-13 05:25:02 -0300

    Added 2e service

--- TP3/src/main/java/com/example/tp3/Repositories/StudentCareerRepository.java
+++ TP3/src/main/java/com/example/tp3/Repositories/StudentCareerRepository.java
@@ -4,9 +4,11 @@ import com.example.tp3.Dtos.CareerReportDto;
 import com.example.tp3.Dtos.CareerWithInscriptionsDto;
 import com.example.tp3.Dtos.StudentByCityDto;
 import com.example.tp3.Models.City;
+import com.example.tp3.Models.Genre;
 import com.example.tp3.Models.StudentCareer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -19,8 +21,11 @@ public interface StudentCareerRepository extends JpaRepository<StudentCareer, In
     public List<CareerWithInscriptionsDto> careersSortedByStudents();
     
     @Query("SELECT NEW com.example.tp3.Dtos.StudentByCityDto(s.name, s.surname) FROM Student s JOIN StudentCareer sc ON s.id = sc.key.studentId WHERE sc.key.careerId = :careerId AND s.city.id = :cityId")
-    public List<StudentByCityDto> studentsByCareerFilteredCyCity(int cityId, int careerId );
+    public List<StudentByCityDto> studentsByCareerFilteredCyCity(@Param ("cityId") int cityId, @Param("careerId") int careerId );
     
+    @Query("SELECT NEW com.example.tp3.Dtos.StudentByCityDto(s.name, s.surname) FROM Student s WHERE s.genre = :genre")
+    public List<StudentByCityDto> studentsByGenre(@Param("genre") Genre genre);
+
     //@Query("") TODO commented because it gives error since the query is not written and can't run the application to continue with other tasks
     //public List<CareerReportDto> careersInformationInscriptionsAndGraduates(); 
 }
