ca7aede62df68a786f62d43db1cd4cfcb57341b6
German De Francesco <germandefrancesco@hotmail.com>
2022-11-07 21:14:45 -0300

    Add TP5_Sales project

---
+++ TP5/src/TP5_Sales/src/main/java/sales/Tp5SalesApplication.java
@@ -0,0 +1,13 @@
+package sales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp5SalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp5SalesApplication.class, args);
+	}
+
+}
