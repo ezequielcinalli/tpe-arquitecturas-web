c107f24adad3e074f1a2441d95366b8c5014b91f
German De Francesco <germandefrancesco@hotmail.com>
2022-11-07 22:01:01 -0300

    Repositories pseudo implementation

--- TP5/src/TP5_Sales/src/main/java/sales/repositories/SaleProductRepository.java
+++ TP5/src/TP5_Sales/src/main/java/sales/repositories/SaleProductRepository.java
@@ -1,5 +1,10 @@
 package sales.repositories;
 
-public class SaleProductRepository {
-	//TODO
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import sales.models.SaleProduct;
+import sales.models.SaleProductId;
+
+@Repository
+public interface SaleProductRepository extends JpaRepository<SaleProduct, SaleProductId> {
 }
