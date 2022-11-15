af158ba9a76b434260a4375cf09722a93a2e9302
MicaCisneros <micaela_cisneros@outlook.es>
2022-11-15 18:08:30 -0300

    Implement JWT in Products microservice and error solution in update

---
+++ TP5/Products/src/main/java/com/tp5/products/Dtos/SignInDto.java
@@ -0,0 +1,9 @@
+package com.tp5.products.Dtos;
+
+public class SignInDto {
+
+	public String user;
+    
+	public String password;
+	
+}
--- TP5/Products/src/main/java/com/tp5/products/ProductsApplication.java
+++ TP5/Products/src/main/java/com/tp5/products/ProductsApplication.java
@@ -2,6 +2,16 @@ package com.tp5.products;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import sales.security.JWTAuthorizationFilter;
+
+@SuppressWarnings("deprecation")
 
 @SpringBootApplication
 public class ProductsApplication {
@@ -9,5 +19,20 @@ public class ProductsApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProductsApplication.class, args);
     }
+
+    
+	@EnableWebSecurity
+	@Configuration
+	class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.csrf().disable()
+				.addFilterAfter(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
+				.authorizeRequests()
+				.antMatchers(HttpMethod.POST, "/auth/sign-in").permitAll()
+				.anyRequest().authenticated();
+		}
+	}
 
 }
